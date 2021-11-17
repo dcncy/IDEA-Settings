d706e6b9771059a4904c167ca61a4e43ee26d36e
jisiqi <jisiqi@ebchinatech.com>
2021-11-17 13:34:28 +0800

    IU-2021.2.3 <dcncy@Dcncy-MBP.local Create markdown.xml, easyJavadoc.xml, project.default.xml, diff.xml, yiiguxing.translation.xml, plantuml.xml, Default.xml, ignore.xml, find.xml, _@user_Darcula.icls, GrepConsole.xml, Default.xml, docker.xml, lightEdit.xml, Dcncy.xml, keymap.xml, OGNL.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, ide-features-trainer.xml, CodeGlance.xml, rainbow_brackets.xml, lombok-plugin.xml, git.xml, shared-indexes.xml, ui.lnf.xml, filetypes.xml, projectView.xml, remote-servers.xml, colors.scheme.xml, External Tools.xml, notifications.xml, AiXCoderConfig_local.xml, databaseDrivers.xml, github.xml, templates.xml, laf.xml, OGNL _Struts 2_.xml, Class.java, jdk.table.xml, mavenRunHelper.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, codota.xml, docker-registry.xml, grazie_global.xml, toStringTemplates.xml, Eclipse _macOS_ copy.xml, mybatis.xml, usageView.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,16 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+
+/**
+ *
+ * @description: ${description}
+ *
+ * @author: JiSiQi <jisiqi@ebchinatech.com>
+ *
+ * @since: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ *
+ */
+public class ${NAME} {
+
+}
